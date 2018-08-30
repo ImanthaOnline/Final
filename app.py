9e3c5fa104f6e1fd3b883d4c5085abef3deb018d
ImanthaOnline <imanthavisal@gmail.com>
2018-08-30 19:18:29 +0530

    Add Face Recognition Part

--- app.py
+++ app.py
@@ -5,20 +5,29 @@ import gc
 
 from flask import Flask, render_template, request, redirect, url_for,jsonify
 from connection import connection
-from passlib.hash import sha256_crypt
+from passlib.hash import sha256_crypt,md5_crypt
 from pymysql import escape_string as thwart
 
 from packages.preprocess import preprocesses
 from packages.classifier import training
 import os
 
+import pickle
+import time
+import cv2
+import numpy as np
+import tensorflow as tf
+from scipy import misc
+from packages import facenet, detect_face
+
 app = Flask(__name__)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 TRAIN_FOLDER = './uploads/train/'
 PRE_FOLDER = './uploads/pre/'
 CLASSIFIER = './class/classifier.pkl'
-MODEL_DIR = './models'
+MODEL_DIR = './model'
 npy = ''
+TEST_FOLDER = './uploads/test/'
 
 
 @app.route('/')
@@ -149,30 +158,131 @@ def criminalinfo():
 
 
 @app.route('/view/', methods=["GET", "POST"])
-def view():
-    try:
-        c, conn = connection()
+def view(filename="img.jpg"):
+    image_path = TEST_FOLDER + filename
+    with tf.Graph().as_default():
+        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.6)
+        sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
+        with sess.as_default():
+            pnet, rnet, onet = detect_face.create_mtcnn(sess, npy)
 
-        nic = "1"
-        data = c.execute("SELECT * FROM  criminalinfo WHERE nic=(%s) ", (thwart(nic)))
-        raw = c.fetchone()
-        print(raw)
-        nic = raw[1]
-        name = raw[2]
-        age = raw[3]
-        address = raw[4]
-        address02 = raw[5]
-        address03 = raw[6]
-        haircolor = raw[7]
-        eyecolor = raw[8]
+            minsize = 20  # minimum size of face
+            threshold = [0.6, 0.7, 0.7]  # three steps's threshold
+            factor = 0.709  # scale factor
+            frame_interval = 3
+            image_size = 182
+            input_image_size = 160
 
-        print(nic)
-        return render_template("view.html", nic=nic, name=name, address=address, address02=address02,
-                               address03=address03, age=age, haircolor=haircolor, eyecolor=eyecolor)
+            HumanNames = os.listdir(TRAIN_FOLDER)
+            HumanNames.sort()
 
+            print('Loading feature extraction model')
+            facenet.load_model(MODEL_DIR)
 
-    except Exception as e:
-        return (str(e))
+            images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
+            embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
+            phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
+            embedding_size = embeddings.get_shape()[1]
+
+            classifier_filename_exp = os.path.expanduser(CLASSIFIER)
+            with open(classifier_filename_exp, 'rb') as infile:
+                (model, class_names) = pickle.load(infile)
+
+            c = 0
+
+            print('Start Recognition!')
+            frame = cv2.imread(image_path, 0)
+
+            frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)  # resize frame (optional)
+
+            timeF = frame_interval
+
+            if (c % timeF == 0):
+
+                if frame.ndim == 2:
+                    frame = facenet.to_rgb(frame)
+                frame = frame[:, :, 0:3]
+                bounding_boxes, _ = detect_face.detect_face(frame, minsize, pnet, rnet, onet, threshold, factor)
+                nrof_faces = bounding_boxes.shape[0]
+                print('Face Detected: %d' % nrof_faces)
+
+                if nrof_faces > 0:
+                    det = bounding_boxes[:, 0:4]
+
+                    cropped = []
+                    scaled = []
+                    scaled_reshape = []
+                    bb = np.zeros((nrof_faces, 4), dtype=np.int32)
+
+                    for i in range(nrof_faces):
+                        emb_array = np.zeros((1, embedding_size))
+
+                        bb[i][0] = det[i][0]
+                        bb[i][1] = det[i][1]
+                        bb[i][2] = det[i][2]
+                        bb[i][3] = det[i][3]
+
+                        # inner exception
+                        if bb[i][0] <= 0 or bb[i][1] <= 0 or bb[i][2] >= len(frame[0]) or bb[i][3] >= len(frame):
+                            print('face is too close')
+                            continue
+
+                        cropped.append(frame[bb[i][1]:bb[i][3], bb[i][0]:bb[i][2], :])
+                        cropped[i] = facenet.flip(cropped[i], False)
+                        scaled.append(misc.imresize(cropped[i], (image_size, image_size), interp='bilinear'))
+                        scaled[i] = cv2.resize(scaled[i], (input_image_size, input_image_size),
+                                               interpolation=cv2.INTER_CUBIC)
+                        scaled[i] = facenet.prewhiten(scaled[i])
+                        scaled_reshape.append(scaled[i].reshape(-1, input_image_size, input_image_size, 3))
+                        feed_dict = {images_placeholder: scaled_reshape[i], phase_train_placeholder: False}
+                        emb_array[0, :] = sess.run(embeddings, feed_dict=feed_dict)
+                        # print("emb_array",emb_array)
+                        predictions = model.predict_proba(emb_array)
+                        print("Predictions ", predictions)
+                        best_class_indices = np.argmax(predictions, axis=1)
+                        best_class_probabilities = predictions[np.arange(len(best_class_indices)), best_class_indices]
+                        print("Best Predictions ", best_class_probabilities)
+
+                        if best_class_probabilities[0] > 0.6:
+                            print('Result Indices: ', best_class_indices[0])
+                            print(HumanNames)
+                            for H_i in HumanNames:
+                                # print(H_i)
+                                if HumanNames[best_class_indices[0]] == H_i:
+                                    result_names = HumanNames[best_class_indices[0]]
+                                    print("Face Recognized: ", result_names)
+                                    return str(result_names)
+                        else:
+                            print('Not Recognized')
+                            return False
+                else:
+                    print('Unable to align')
+                    return False
+
+    return False
+
+def random_name():
+    name = md5_crypt.encrypt(str(time.time())).split("$")[2]
+    return name
+
+@app.route('/identify/', methods=['POST'])
+def authenticateUser():
+
+    target = os.path.join(APP_ROOT, 'uploads/test/')
+    if not os.path.isdir(target):
+        os.mkdir(target)
+
+    filename = random_name() + ".png"
+    destination = "/".join([target, filename])
+
+    for file in request.files.getlist('img'):
+        print(filename)
+        file.save(destination)
+
+    result = view(filename)
+    os.remove(destination)
+
+    return 'test'
 
 
 @app.route('/search/', methods=["GET", "POST"])
@@ -201,6 +311,9 @@ def search():
 
     return render_template("regi.html")
 
+@app.route('/find/', methods=["GET", "POST"])
+def find():
+    return render_template("view.html")
 
 if __name__ == "__main__":
     app.run()
