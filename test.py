23a49bd2e92c062a81348bb371ddf8227b02a903
ImanthaOnline <imanthavisal@gmail.com>
2018-08-30 21:30:07 +0530

    Unit Testing

--- test.py
+++ test.py
@@ -9,10 +9,31 @@ class FlaskTestCase(unittest.TestCase):
         response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
-    def test_user_login_page_load(self):
+    def search(self):
         tester = app.test_client(self)
-        response = tester.get('/login/', content_type='html/text')
+        response = tester.get('/search/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
+
+    def find(self):
+        tester = app.test_client(self)
+        response = tester.get('/find/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    def identify(self):
+            tester = app.test_client(self)
+            response = tester.get('/identify/', content_type='html/text')
+            self.assertEqual(response.status_code, 200)
+
+    def view(self):
+            tester = app.test_client(self)
+            response = tester.get('/view/', content_type='html/text')
+            self.assertEqual(response.status_code, 200)
+
+
+    def criminalinfo(self):
+            tester = app.test_client(self)
+            response = tester.get('/criminalinfo/', content_type='html/text')
+            self.assertEqual(response.status_code, 200)
 
 
 
